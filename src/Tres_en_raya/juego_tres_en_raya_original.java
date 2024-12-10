3edebb6acaee88982047cca3f9ec11f4f0c62c05
ruben1421 <rubenantoniomunoz0@gmail.com>
2024-12-10 17:18:25 +0100

    Comentario de Tres en Raya
    Comentar y mejorar el Código de tres en Raya, aparte se le agrego para preguntar si los jugadores quieren volver ha jugar

--- src/Tres_en_raya/juego_tres_en_raya_original.java
+++ src/Tres_en_raya/juego_tres_en_raya_original.java
@@ -7,69 +7,93 @@ public class juego_tres_en_raya_original {
 
 	public static void main(String[] args) {
 	
-
 		
-	
+		//Damos la bienvenida al juego del Colgado 
 
 		   System.out.println("BIENVENIDOS AL JUEGO DE TRES EN RAYA ");
 		   
 		    Scanner s = new Scanner(System.in);
 
-	      
-	        boolean jugarDeNuevo = true;
+	      //Aqui indicamos que si al finalizar el juego, ambos jugadores escogen jugar de nuevo se vuelva
+		    // ha ejecutar el juego.
+	       
+		    
+		    
+		    
+		    boolean jugarDeNuevo = true;
 	        while (jugarDeNuevo) {
 	            
+	        // Aqui es donde se inicia el juego, pidiendo que cada jugador ingrese un nombre 	
+	        
 	            System.out.print("Jugador 1, ingresa tu nombre: ");
 	            String nombreJugador1 = s.nextLine();
 	            System.out.print("Jugador 2, ingresa tu nombre: ");
 	            String nombreJugador2 = s.nextLine();
 
-	         
+	         //Introducimos el tablero que en este caso para el juego es una matriz 
+	            //3x3 
+	            
 	            char[][] tablero = new char[3][3];
 	            for (int i = 0; i < 3; i++) {
 	                for (int j = 0; j < 3; j++) {
-	                    tablero[i][j] = '_'; 
+	                    tablero[i][j] = '_'; // Aqui Indicamos que los elementos del tablero se reemplace 
+	                    						//por '_' para representar el tablero vacio
 	                }
 	            }
-
+	            
+	            // En este apartado se realiza la eleccion de los jugadores por cara o sello para poder saber
+	            // quien empieza el juego
 	         
 	            System.out.println(nombreJugador1 + ", elige cara (1) o sello (2): ");
 	            int eleccionJugador1 = s.nextInt();
 
-	           
+	           //Aqui indicamos que si eleccionJugador1 = 1 la eleccionJugador2= 2 si esto no se cumple 
+	            //eleccionJugador =1 
+	            
+	            // utilizamos el ? para decir ? true : false
+	            
 	            int eleccionJugador2 = (eleccionJugador1 == 1) ? 2 : 1;
 
-	            // Mostrar las elecciones
+	            // En este apartado imprimimos la eleccion de los juagdores
+	            
 	            System.out.println(nombreJugador1 + " ha elegido: " + (eleccionJugador1 == 1 ? "CARA" : "SELLO"));
 	            System.out.println(nombreJugador2 + " ha elegido: " + (eleccionJugador2 == 1 ? "CARA" : "SELLO"));
 
-	           
+	           //Aqui incorporamos un Random para que se pueda hacer el sorteo, simulando el lanzamiento de una moneda 
+	            // y a quien le toca el turno de manera al azar
+	            
 	            Random random = new Random();
 	            int resultadoSorteo = random.nextInt(2) + 1;
 
-	          
+	          // Aqui indicamos que si en el sorteo salio 1cara decimos que comienza el nombreJugador1 y si no el 
+	            // El nombreJugador2
+	            
 	            if (resultadoSorteo == 1) {
 	                System.out.println("¡El sorteo ha determinado que " + nombreJugador1 + " comienza con CARA ('X')!");
 	            } else {
 	                System.out.println("¡El sorteo ha determinado que " + nombreJugador2 + " comienza con SELLO ('O')!");
 	            }
-
+	            
+	            //Aqui indicamos que si resultado sorteo es 1 puede tocarle X o O
 	          
 	            char jugadorActual = (resultadoSorteo == 1) ? 'X' : 'O'; 
 
-	         
+	            //Aqui incorporamos el espacio entre las lineas del tres en raya 
 	            boolean juegoActivo = true;
 	            while (juegoActivo) {
 	               
 	                System.out.println("Tablero actual:");
 	                for (int i = 0; i < 3; i++) {
 	                    for (int j = 0; j < 3; j++) {
-	                        System.out.print(tablero[i][j] + " ");  
+	                        System.out.print(tablero[i][j] + " ");  // Aqui decimos que se separe 
+	                        										// Con un espacio la matriz del tablero 
 	                    }
 	                    System.out.println(); 
 	                }
 
-	               
+	                //Aqui imprimimos el turno del jugador y de la misma manera le da la opcion de ingresar
+	                //la posicion de su ficha en el tablero indicandole que ingrese el numero de filas 
+	                //y de Columnas.
 	                String jugadorNombre = (jugadorActual == 'X') ? nombreJugador1 : nombreJugador2;
 	                System.out.println("Turno de " + jugadorNombre + " (" + jugadorActual + ")");
 	                System.out.print("Ingresa la fila (0-2): ");
@@ -77,7 +101,9 @@ public class juego_tres_en_raya_original {
 	                System.out.print("Ingresa la columna (0-2): ");
 	                int columna = s.nextInt();
 
-	           
+	                //Aqui indicamos que si uno de los jugadores escoge una posicion que ya halla sido 
+	                // elegida por el otro jugador, le indique que la casilla ya esta ocupada y que elija otra.
+	                
 	                if (tablero[fila][columna] == '_') {
 	                    
 	                    tablero[fila][columna] = jugadorActual;
@@ -89,7 +115,11 @@ public class juego_tres_en_raya_original {
 	                
 	                boolean haGanado = false;
 
+	                // En este apartado indicamos que si el jugador llena la array, tanto de forma vertical, horizontal o diagonal
+	                // le reconozca que ha ganado el juego 
 	               
+	              //Jugador hacerto en columna
+	                
 	                for (int i = 0; i < 3; i++) {
 	                    if (tablero[i][0] == jugadorActual && tablero[i][1] == jugadorActual && tablero[i][2] == jugadorActual) {
 	                        haGanado = true;
@@ -97,7 +127,8 @@ public class juego_tres_en_raya_original {
 	                    }
 	                }
 
-	           
+	                //Jugador hacerto en Fila 
+	                
 	                if (!haGanado) {
 	                    for (int j = 0; j < 3; j++) {
 	                        if (tablero[0][j] == jugadorActual && tablero[1][j] == jugadorActual && tablero[2][j] == jugadorActual) {
@@ -107,7 +138,8 @@ public class juego_tres_en_raya_original {
 	                    }
 	                }
 
-	             
+	              //Jugador hacerto en Diagonal
+	                
 	                if (!haGanado) {
 	                    if (tablero[0][0] == jugadorActual && tablero[1][1] == jugadorActual && tablero[2][2] == jugadorActual) {
 	                        haGanado = true;
@@ -117,12 +149,14 @@ public class juego_tres_en_raya_original {
 	                    }
 	                }
 
-	             
+	             //Aqui imprimimos cual de los juagadores ha ganado 
+	                
 	                if (haGanado) {
 	                    System.out.println("¡El jugador " + jugadorNombre + " ha ganado!");
 	                    juegoActivo = false;
 	                }
-
+	                
+	                //Aqui indicamos cuando ocurre un empate 
 	                
 	                boolean empate = true;
 	                for (int i = 0; i < 3; i++) {
@@ -144,14 +178,34 @@ public class juego_tres_en_raya_original {
 	                    jugadorActual = 'X';
 	                }
 	            }
+	            
+	            // En este apartado preguntamos a los juagdores si quieren volver a jaugar o no 
+	            
 	            System.out.print(nombreJugador1 + ", ¿quieres jugar otra vez? (si/no): ");
 	            String respuestaJugador1 = s.next();
 	            System.out.print(nombreJugador2 + ", ¿quieres jugar otra vez? (si/no): ");
 	            String respuestaJugador2 = s.next();
         
+	            //Si los dos jugadores deciden que si se inicia un nuevo juego 
+	            
+	            if (respuestaJugador1.equalsIgnoreCase("si") && respuestaJugador2.equalsIgnoreCase("si")) {
+	                jugarDeNuevo = true;
+	                System.out.println("¡Comencemos una nueva partida!");
+	            } else {
+	            	
+	            	// Si no se da el juego por terminado 
+	            	
+	                jugarDeNuevo = false;
+	                System.out.println("Gracias por jugar. ¡Hasta la próxima!");
+	            }
+	            
 	    }
 	    
 	}
+	
+	
+	
+	
 	
 }
 
